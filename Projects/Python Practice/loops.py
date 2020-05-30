4543637b4a224661b8e92c6f9023cd4faca8d2e1
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-05-30 16:20:20 -0400

    Create loops.py

---
+++ Projects/Python Practice/loops.py
@@ -0,0 +1,34 @@
+#Loops
+
+"""
+#For loops
+#One type of parameter
+for num in range (50): #start, end
+    print(num)
+    
+
+#two types of parameters
+for num in range (0,50): #start and end
+    print (num)
+"""
+
+#three types of parameters #start, end, interval
+for num in range (0,101,2):
+    print(num)
+
+    
+#while loop
+
+password= "MASSEYHACKS"
+field= ""
+while field!=password:
+    field= input ("PASSWORD")
+print("You got it!")
+
+
+#for each loop
+
+string= "OK BOOMER"
+for character in string:
+    print(character)
+
