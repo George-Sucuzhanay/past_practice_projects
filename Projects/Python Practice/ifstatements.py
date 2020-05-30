fb1989ef0c72bc52f71d70030f0f2bfade9a2c9c
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-05-30 16:21:20 -0400

    Create ifstatements.py

---
+++ Projects/Python Practice/ifstatements.py
@@ -0,0 +1,17 @@
+#If statement start off the logic
+if mark == 100:
+    ans = "A++"
+    print("Wow you're a great scholar")
+
+elif 80<=mark:
+    ans ="A"
+    
+elif  60<-mark:
+    ans ="B"
+
+else:
+    ans ="fail"
+
+
+print (ans, "is the letter grade you got")
+
