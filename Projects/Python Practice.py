fb6d42c00f97269d3bdf9f1196a7c3122c114311
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-05-30 15:17:00 -0400

    Update and rename Python Learning.py to Python Practice.py

--- Projects/Python Practice.py
+++ Projects/Python Practice.py
@@ -2,6 +2,8 @@
 
 print("Hello World")
 
+print ("George Sucuzhanay")
+
 print ("High School Student")
 
 #Variables
@@ -40,3 +42,33 @@ string = "Hello " +"World"
 print (string)
 
 print("Hello" *10) #Print hello ten times
+
+#VARIABLE TYPES:
+"""
+String 'hello'
+
+Integer 5 , 6, -4
+
+Float 5.6, 10.1
+
+Boolean True/False
+
+"""
+prcieOfPhone= 500.24335
+
+didLaundry = True
+didLaundry = False
+
+integer = int("5") #converts any variable type to integer
+
+floatnum= float ("5.5435") #convert any variable type to float
+
+coolstring = str(47332) #Converts any variable type to string
+
+
+
+print (integer+floatnum)
+
+
+
+
