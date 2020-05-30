18d0938dcc24ab53b1e7afc3e48678f0bc091c64
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-05-30 14:46:08 -0400

    Create Python Learning.py

---
+++ Projects/Python Learning.py
@@ -0,0 +1,42 @@
+#This is our first Python Project
+
+print("Hello World")
+
+print ("High School Student")
+
+#Variables
+#Storing a string variable
+x= ("Hello World")
+#Storing ainterger variable
+numofApples=4
+
+print(x)
+print(numofApples)
+
+#INTEGER TYPE
+
+#Addition
+num = 5+5 #num will be equal to 10
+print(num)
+
+#Subtraction
+num = 10-9 # =-1
+print (num)
+
+#Multiplication
+num = 2*3
+print (num)
+
+#Divison
+num = 100/5
+print (num)
+
+#Remainder
+num = 10%3 #1
+print (num)
+
+#STRING
+string = "Hello " +"World"
+print (string)
+
+print("Hello" *10) #Print hello ten times
