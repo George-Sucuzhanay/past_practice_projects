84daba58e0531a3affa41aaae22e69c8ea4f5c4e
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-07-10 16:42:20 -0400

    Working on with JavaScript with All Star Code

---
+++ Projects/ASCProjects/week1/day3/conditionals_project.js
@@ -0,0 +1,13 @@
+let boba = 5;
+
+if (boba <3)) {
+    console.log ('!');
+}
+
+let userinput =process.argv [0];
+let userinput = process.argv [1];
+let userinput = process.argv [2];
+
+console.log("Input A:", firstInput);
+console.log("Input B:", secondInput);
+console.log("Input C:", thirdInput);
