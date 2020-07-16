b3478a51f957f73b1e363eb5c16477d8c680fbb2
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-07-16 16:27:52 -0400

    Create test.py

---
+++ Projects/Python Practice/test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+    DIRECTORY = "reports"
+    NAME = "PS4"
+    CURRENCY = "$"
+    MIN_PRICE = "275"
+    MAX_PRICE = "650"
+    FILTERS = {
+        "min": MIN_PRICE,
+        "max": MAX_PRICE
+    }
+    BASE_URL: "http:/www.amazon.de/"
+    href get_chrome_web_driver(options):
+        return webdriver.ChromeOptions()
